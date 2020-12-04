3bef3c22208266352047269e8986cf3f25cf1780
Taylor Fah <taylorfah@Taylors-MacBook-Pro.local>
2020-12-04 10:29:56 -0800

    added comments to middleware

--- src/lib/commonMiddleware.js
+++ src/lib/commonMiddleware.js
@@ -5,7 +5,7 @@ import httpErrorHandler from "@middy/http-error-handler";
 
 export default handler => middy(handler)
     .use([
-        httpJsonBodyParser(),
-        httpEventNormalizer(),
-        httpErrorHandler(),
+        httpJsonBodyParser(), // automatically parses HTTP requests with a JSON body and converts the body into an object
+        httpEventNormalizer(), // gives access to query params in event.queryStringParameters
+        httpErrorHandler(), // Automatically handles uncaught errors that contain the properties statusCode (number) and message (string) and creates a proper HTTP response
     ]);
